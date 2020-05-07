b9ac3e8cfdc739e049f79bdddae5e4ebe61672d1
muneral <zhuchenyi@bupt.edu.cn>
2020-05-08 01:31:22 +0800

    homework

--- Homework/app/src/main/java/com/byted/chapter5/ApiService.java
+++ Homework/app/src/main/java/com/byted/chapter5/ApiService.java
@@ -1,16 +1,27 @@
 package com.byted.chapter5;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
     // https://wanandroid.com/wxarticle/chapters/json
     @GET("wxarticle/chapters/json")
     Call<ArticleResponse> getArticles();
+    //@GET("wxarticle/chapters/json?user=……")
+    //Call<ArticleResponse> getArticles(@Query("user") String name);
 
     // todo 添加api
     // https://www.wanandroid.com/user/register
     //方法：POST
     //	username,password,repassword
+    @FormUrlEncoded
+    @POST("user/register")
+    Call<UserResponse> register(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("repassword") String repassword);
 
 }
--- Homework/app/src/main/java/com/byted/chapter5/User.java
+++ Homework/app/src/main/java/com/byted/chapter5/User.java
@@ -19,5 +19,6 @@ import com.google.gson.annotations.SerializedName;
 class User {
     @SerializedName("nickname")
     public String nickname;
-
+    @SerializedName("id")
+    public int ID;
 }
